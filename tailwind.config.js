f7c0cfa5c31461a79a55e12b058fb424ccf98171
LUCAS GOMES BERNARDINO <lucas.bernardino@premoldados.local>
2024-10-16 16:23:35 -0300

    adiciona home

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts,scss}" // Inclua os arquivos SCSS, HTML e TypeScript
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
